3a31cfc541a2538330979b8311c96e75fa5ea4fa
Takayoshi Kochi <takayoshi.kochi@gmail.com>
2023-06-23 00:11:30 +0900

    Fix include filename in z80debug.cpp
    Use lower-case filename.

--- src/devices/z80debug.cpp
+++ src/devices/z80debug.cpp
@@ -7,7 +7,7 @@
 //  $Id: Z80Debug.cpp,v 1.5 1999/07/29 14:35:23 cisc Exp $
 
 #include "win32/headers.h"
-#include "devices/z80Debug.h"
+#include "devices/z80debug.h"
 #include "common/device_i.h"
 
 Z80Debug* Z80Debug::currentcpu = 0;
