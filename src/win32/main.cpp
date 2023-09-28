5003d44733c4ca2832258064f8d83481417f38e8
Takayoshi Kochi <takayoshi.kochi@gmail.com>
2023-09-28 11:34:53 +0900

    Update common controls visual style.

--- src/win32/main.cpp
+++ src/win32/main.cpp
@@ -11,6 +11,23 @@
 #include "win32/ui.h"
 #include "win32/file.h"
 
+
+// Use modern visual style in common controls
+// https://learn.microsoft.com/ja-jp/windows/win32/controls/cookbook-overview
+#if defined _M_IX86
+#pragma comment( \
+        linker,  \
+            "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment( \
+        linker,  \
+            "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment( \
+        linker,  \
+            "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+
 // ---------------------------------------------------------------------------
 
 char m88dir[MAX_PATH];
