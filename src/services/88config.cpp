f267166dfa6ce3c96c605f78bbf3054513c3df59
Takayoshi Kochi <takayoshi.kochi@gmail.com>
2023-10-29 14:21:18 +0900

    clean up applydefault.

--- src/services/88config.cpp
+++ src/services/88config.cpp
@@ -30,11 +30,10 @@ void InitPathInfo() {
 bool LoadConfigEntry(const std::string_view inifile,
                      const char* entry,
                      int* value,
-                     int default_value,
-                     bool apply_default) {
+                     int default_value) {
   int n = GetPrivateProfileInt(AppName, entry, -1, inifile.data());
 
-  if (n == -1 && apply_default)
+  if (n == -1)
     n = default_value;
   if (n != -1) {
     *value = n;
@@ -46,14 +45,12 @@ bool LoadConfigEntry(const std::string_view inifile,
 bool LoadConfigEntryU(const std::string_view inifile,
                       const char* entry,
                       uint32_t* value,
-                      uint32_t default_value,
-                      bool apply_default) {
+                      uint32_t default_value) {
   int n = GetPrivateProfileInt(AppName, entry, -1, inifile.data());
-  auto r = static_cast<uint32_t>(n);
-  if (n == -1 && apply_default)
-    r = default_value;
+  if (n == -1)
+    *value = default_value;
   if (n != -1) {
-    *value = r;
+    *value = static_cast<uint32_t>(n);
     return true;
   }
   return false;
@@ -67,107 +64,92 @@ std::string LoadConfigString(const std::string_view inifile, const char* entry) 
   return {};
 }
 
-bool SaveEntry(const std::string_view inifile, const char* entry, int value, bool applydefault) {
+bool SaveEntry(const std::string_view inifile, const char* entry, int value) {
   char buf[MAX_PATH];
-  if (applydefault || -1 != GetPrivateProfileInt(AppName, entry, -1, inifile.data())) {
-    wsprintf(buf, "%d", value);
-    WritePrivateProfileString(AppName, entry, buf, inifile.data());
-    return true;
-  }
-  return false;
+  wsprintf(buf, "%d", value);
+  WritePrivateProfileString(AppName, entry, buf, inifile.data());
+  return true;
 }
 
-bool SaveEntry(const std::string_view inifile,
-               const char* entry,
-               const char* value,
-               bool applydefault) {
-  bool apply = applydefault;
-  if (!applydefault) {
-    char buf[8];
-    GetPrivateProfileString(AppName, entry, ";", buf, sizeof(buf), inifile.data());
-    if (buf[0] != ';')
-      apply = true;
-  }
-  if (apply)
-    WritePrivateProfileString(AppName, entry, value, inifile.data());
-  return apply;
+bool SaveEntry(const std::string_view inifile, const char* entry, const char* value) {
+  WritePrivateProfileString(AppName, entry, value, inifile.data());
+  return true;
 }
 
 // TODO:
 constexpr int VOLUME_BIAS = 100;
 
-void SaveConfig(pc8801::Config* cfg, const std::string_view inifile, bool writedefault) {
+void SaveConfig(const pc8801::Config* cfg, const std::string_view inifile) {
   char buf[MAX_PATH];
   GetCurrentDirectory(MAX_PATH, buf);
-  SaveEntry(inifile, "Directory", buf, writedefault);
+  SaveEntry(inifile, "Directory", buf);
 
-  SaveEntry(inifile, "Flags", cfg->flags, writedefault);
-  SaveEntry(inifile, "Flag2", cfg->flag2, writedefault);
-  SaveEntry(inifile, "CPUClock", cfg->legacy_clock, writedefault);
-  // SaveEntry(inifile, "Speed", cfg->speed, writedefault);
+  SaveEntry(inifile, "Flags", cfg->flags);
+  SaveEntry(inifile, "Flag2", cfg->flag2);
+  SaveEntry(inifile, "CPUClock", cfg->legacy_clock);
+  // SaveEntry(inifile, "Speed", cfg->speed);
   // Obsolete - always refresh.
-  // SaveEntry(inifile, "RefreshTiming", cfg->refreshtiming, writedefault);
-  SaveEntry(inifile, "BASICMode", static_cast<int>(cfg->basicmode), writedefault);
-  SaveEntry(inifile, "Sound", cfg->sound_output_hz, writedefault);
-  SaveEntry(inifile, "Switches", cfg->dipsw, writedefault);
-  SaveEntry(inifile, "SoundBuffer", cfg->sound_buffer_ms, writedefault);
-  SaveEntry(inifile, "MouseSensibility", cfg->mousesensibility, writedefault);
-  SaveEntry(inifile, "CPUMode", cfg->cpumode, writedefault);
-  SaveEntry(inifile, "KeyboardType", static_cast<int>(cfg->keytype), writedefault);
-  SaveEntry(inifile, "ERAMBank", cfg->erambanks, writedefault);
+  // SaveEntry(inifile, "RefreshTiming", cfg->refreshtiming);
+  SaveEntry(inifile, "BASICMode", static_cast<int>(cfg->basicmode));
+  SaveEntry(inifile, "Sound", cfg->sound_output_hz);
+  SaveEntry(inifile, "Switches", cfg->dipsw);
+  SaveEntry(inifile, "SoundBuffer", cfg->sound_buffer_ms);
+  SaveEntry(inifile, "MouseSensibility", cfg->mousesensibility);
+  SaveEntry(inifile, "CPUMode", cfg->cpumode);
+  SaveEntry(inifile, "KeyboardType", static_cast<int>(cfg->keytype));
+  SaveEntry(inifile, "ERAMBank", cfg->erambanks);
 
   // Obsolete
-  // SaveEntry(inifile, "LPFCutoff", cfg->lpffc, writedefault);
-  // SaveEntry(inifile, "LPFOrder", static_cast<int>(cfg->lpforder), writedefault);
+  // SaveEntry(inifile, "LPFCutoff", cfg->lpffc);
+  // SaveEntry(inifile, "LPFOrder", static_cast<int>(cfg->lpforder));
 
-  SaveEntry(inifile, "ROMEOLatency", cfg->romeolatency, writedefault);
+  SaveEntry(inifile, "ROMEOLatency", cfg->romeolatency);
 
-  SaveEntry(inifile, "VolumeFM", cfg->volfm + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeSSG", cfg->volssg + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeADPCM", cfg->voladpcm + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeRhythm", cfg->volrhythm + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeBD", cfg->volbd + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeSD", cfg->volsd + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeTOP", cfg->voltop + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeHH", cfg->volhh + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeTOM", cfg->voltom + VOLUME_BIAS, writedefault);
-  SaveEntry(inifile, "VolumeRIM", cfg->volrim + VOLUME_BIAS, writedefault);
+  SaveEntry(inifile, "VolumeFM", cfg->volfm + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeSSG", cfg->volssg + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeADPCM", cfg->voladpcm + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeRhythm", cfg->volrhythm + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeBD", cfg->volbd + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeSD", cfg->volsd + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeTOP", cfg->voltop + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeHH", cfg->volhh + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeTOM", cfg->voltom + VOLUME_BIAS);
+  SaveEntry(inifile, "VolumeRIM", cfg->volrim + VOLUME_BIAS);
 
-  SaveEntry(inifile, "WinPosY", cfg->winposy, writedefault);
-  SaveEntry(inifile, "WinPosX", cfg->winposx, writedefault);
+  SaveEntry(inifile, "WinPosY", cfg->winposy);
+  SaveEntry(inifile, "WinPosX", cfg->winposx);
 
-  SaveEntry(inifile, "SoundDriverType", static_cast<int>(cfg->sound_driver_type), writedefault);
-  SaveEntry(inifile, "PreferredASIODriver", cfg->preferred_asio_driver.c_str(), writedefault);
+  SaveEntry(inifile, "SoundDriverType", static_cast<int>(cfg->sound_driver_type));
+  SaveEntry(inifile, "PreferredASIODriver", cfg->preferred_asio_driver.c_str());
 }
 
-#define LOADVOLUMEENTRY(key, def, vol)                      \
-  if (LoadConfigEntry(inifile, key, &n, def, applydefault)) \
+#define LOADVOLUMEENTRY(key, def, vol)        \
+  if (LoadConfigEntry(inifile, key, &n, def)) \
     vol = n - VOLUME_BIAS;
 
-void LoadConfig(pc8801::Config* cfg, const std::string_view inifile, bool applydefault) {
+void LoadConfig(pc8801::Config* cfg, const std::string_view inifile) {
   uint32_t u =
       pc8801::Config::kSubCPUControl | pc8801::Config::kSaveDirectory | pc8801::Config::kEnableWait;
-  LoadConfigEntryU(inifile, "Flags", &cfg->flags, u, applydefault);
+  LoadConfigEntryU(inifile, "Flags", &cfg->flags, u);
   cfg->flags &= ~pc8801::Config::kSpecialPalette;
 
   u = 0;
-  LoadConfigEntryU(inifile, "Flag2", &cfg->flag2, u, applydefault);
+  LoadConfigEntryU(inifile, "Flag2", &cfg->flag2, u);
   cfg->flag2 &= ~(pc8801::Config::kMask0 | pc8801::Config::kMask1 | pc8801::Config::kMask2);
 
   int n = 0;
-  if (LoadConfigEntry(inifile, "CPUClock", &n, 40, applydefault))
+  if (LoadConfigEntry(inifile, "CPUClock", &n, 40))
     cfg->legacy_clock = Limit(n, 1000, 1);
 
-  //  if (LoadConfigEntry(inifile, "Speed", &n, 1000, applydefault))
+  //  if (LoadConfigEntry(inifile, "Speed", &n, 1000))
   //      cfg->speed = Limit(n, 2000, 500);
   cfg->speed = 1000;
 
   // Obsolete
-  // if (LoadConfigEntry(inifile, "RefreshTiming", &n, 1, applydefault))
+  // if (LoadConfigEntry(inifile, "RefreshTiming", &n, 1))
   //   cfg->refreshtiming = Limit(n, 4, 1);
 
-  if (LoadConfigEntry(inifile, "BASICMode", &n, static_cast<int>(pc8801::BasicMode::kN88V2),
-                      applydefault)) {
+  if (LoadConfigEntry(inifile, "BASICMode", &n, static_cast<int>(pc8801::BasicMode::kN88V2))) {
     pc8801::BasicMode bm = static_cast<pc8801::BasicMode>(n);
     if (bm == pc8801::BasicMode::kN80 || bm == pc8801::BasicMode::kN88V1 ||
         bm == pc8801::BasicMode::kN88V1H || bm == pc8801::BasicMode::kN88V2 ||
@@ -178,14 +160,14 @@ void LoadConfig(pc8801::Config* cfg, const std::string_view inifile, bool applyd
       cfg->basicmode = pc8801::BasicMode::kN88V2;
   }
 
-  if (LoadConfigEntry(inifile, "SoundDriverType", &n, 0, applydefault)) {
+  if (LoadConfigEntry(inifile, "SoundDriverType", &n, 0)) {
     if (n >= pc8801::Config::SoundDriverType::kNumSoundDriverTypes)
       cfg->sound_driver_type = pc8801::Config::SoundDriverType::kUnknown;
   }
 
   cfg->preferred_asio_driver = LoadConfigString(inifile, "PreferredASIODriver");
 
-  if (LoadConfigEntry(inifile, "Sound", &n, 55467, applydefault)) {
+  if (LoadConfigEntry(inifile, "Sound", &n, 55467)) {
     static const uint32_t srate[] = {0, 11025, 22050, 44100, 44100, 48000, 55467};
     if (n < 7)
       cfg->sound_output_hz = srate[n];
@@ -194,36 +176,36 @@ void LoadConfig(pc8801::Config* cfg, const std::string_view inifile, bool applyd
   }
 
   // Obsolete
-  // if (LoadConfigEntry(inifile, "OPNClock", &n, 3993600, applydefault))
+  // if (LoadConfigEntry(inifile, "OPNClock", &n, 3993600))
   //   cfg->opnclock = Limit(n, 10000000, 1000000);
 
-  if (LoadConfigEntry(inifile, "ERAMBank", &n, 4, applydefault))
+  if (LoadConfigEntry(inifile, "ERAMBank", &n, 4))
     cfg->erambanks = Limit(n, 256, 0);
 
-  if (LoadConfigEntry(inifile, "KeyboardType", &n, 0, applydefault))
+  if (LoadConfigEntry(inifile, "KeyboardType", &n, 0))
     cfg->keytype = static_cast<pc8801::KeyboardType>(n);
   if (cfg->keytype == pc8801::KeyboardType::kPC98_obsolete)
     cfg->keytype = pc8801::KeyboardType::kAT106;
 
-  if (LoadConfigEntry(inifile, "Switches", &n, 1829, applydefault))
+  if (LoadConfigEntry(inifile, "Switches", &n, 1829))
     cfg->dipsw = n;
 
-  if (LoadConfigEntry(inifile, "SoundBuffer", &n, 50, applydefault))
+  if (LoadConfigEntry(inifile, "SoundBuffer", &n, 50))
     cfg->sound_buffer_ms = Limit(n, 1000, 20);
 
-  if (LoadConfigEntry(inifile, "MouseSensibility", &n, 4, applydefault))
+  if (LoadConfigEntry(inifile, "MouseSensibility", &n, 4))
     cfg->mousesensibility = Limit(n, 10, 1);
 
-  if (LoadConfigEntry(inifile, "CPUMode", &n, pc8801::Config::kMainSubAuto, applydefault))
+  if (LoadConfigEntry(inifile, "CPUMode", &n, pc8801::Config::kMainSubAuto))
     cfg->cpumode = Limit(n, 2, 0);
 
-  //  if (LoadConfigEntry(inifile, "LPFCutoff", &n, 8000, applydefault))
+  //  if (LoadConfigEntry(inifile, "LPFCutoff", &n, 8000))
   //    cfg->lpffc = Limit(n, 24000, 3000);
   //
-  //  if (LoadConfigEntry(inifile, "LPFOrder", &n, 4, applydefault))
+  //  if (LoadConfigEntry(inifile, "LPFOrder", &n, 4))
   //    cfg->lpforder = Limit(n, 16, 2);
 
-  if (LoadConfigEntry(inifile, "ROMEOLatency", &n, 100, applydefault))
+  if (LoadConfigEntry(inifile, "ROMEOLatency", &n, 100))
     cfg->romeolatency = Limit(n, 500, 0);
 
   LOADVOLUMEENTRY("VolumeFM", VOLUME_BIAS, cfg->volfm);
@@ -237,8 +219,8 @@ void LoadConfig(pc8801::Config* cfg, const std::string_view inifile, bool applyd
   LOADVOLUMEENTRY("VolumeTOM", VOLUME_BIAS, cfg->voltom);
   LOADVOLUMEENTRY("VolumeRIM", VOLUME_BIAS, cfg->volrim);
 
-  LoadConfigEntry(inifile, "WinPosY", &cfg->winposy, 64, applydefault);
-  LoadConfigEntry(inifile, "WinPosX", &cfg->winposx, 64, applydefault);
+  LoadConfigEntry(inifile, "WinPosY", &cfg->winposy, 64);
+  LoadConfigEntry(inifile, "WinPosX", &cfg->winposx, 64);
 }
 }  // namespace
 
@@ -250,11 +232,11 @@ std::once_flag ConfigService::once_;
 // static
 void ConfigService::Init() {
   InitPathInfo();
-  LoadConfig(&instance_.config_, m88ini, true);
+  LoadConfig(&instance_.config_, m88ini);
 }
 
 void ConfigService::Save() {
-  SaveConfig(&config_, m88ini, true);
+  SaveConfig(&config_, m88ini);
 }
 
 void ConfigService::LoadConfigDirectory(const char* entry, bool readalways) {
