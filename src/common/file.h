8556adb5dfd2537c095a503e0b90c1d45404b3b9
Takayoshi Kochi <takayoshi.kochi@gmail.com>
2024-01-13 16:11:30 +0900

    common/file cleanup

--- src/common/file.h
+++ src/common/file.h
@@ -1,76 +1,57 @@
 // ---------------------------------------------------------------------------
 // M88 - PC8801 Series Emulator
 // Copyright (C) by cisc 1998, 2003.
+//
+// generic file io class
 // ---------------------------------------------------------------------------
+//  $Id: file.h,v 1.6 1999/11/26 10:14:09 cisc Exp $
 
 #pragma once
 
 #include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string_view>
 
 class FileIO {
  public:
   enum Flags {
-    open = 0x000001,
-    readonly = 0x000002,
-    create = 0x000004,
+    kOpen = 0x000001,
+    kReadOnly = 0x000002,
+    kCreate = 0x000004,
   };
 
   enum SeekMethod {
-    begin = 0,
-    current = 1,
-    end = 2,
+    kBegin = 0,
+    kCurrent = 1,
+    kEnd = 2,
   };
 
-  enum Error { success = 0, file_not_found, sharing_violation, unknown = -1 };
+  enum Error { kSuccess = 0, kFileNotFound, kSharingViolation, kUnknown = -1 };
 
- public:
   FileIO() = default;
-  virtual ~FileIO() = default;
+  ~FileIO() { Close(); }
 
-  virtual bool Open(const std::string_view filename, uint32_t flg) = 0;
-  virtual bool CreateNew(const std::string_view filename) = 0;
-  virtual bool Reopen(uint32_t flg) = 0;
-  virtual void Close() = 0;
+  FileIO(const FileIO&) = delete;
+  const FileIO& operator=(const FileIO&) = delete;
+
+  bool Open(const std::string_view filename, uint32_t flg);
+  bool CreateNew(const std::string_view filename);
+  bool Reopen(uint32_t flg);
+  void Close();
   [[nodiscard]] Error GetError() const { return error_; }
 
-  virtual int32_t Read(void* dest, int32_t len) = 0;
-  virtual int32_t Write(const void* src, int32_t len) = 0;
-  virtual bool Seek(int32_t fpos, SeekMethod method) = 0;
-  virtual int32_t Tellp() = 0;
-  virtual bool SetEndOfFile() = 0;
+  int32_t Read(void* dest, int32_t len);
+  int32_t Write(const void* src, int32_t len);
+  bool Seek(int32_t fpos, SeekMethod method);
+  int32_t Tellp();
+  bool SetEndOfFile();
 
   [[nodiscard]] uint32_t GetFlags() const { return flags_; }
   void SetLogicalOrigin(int32_t origin) { origin_ = origin; }
 
- protected:
-  uint32_t flags_ = 0;
-  uint32_t origin_ = 0;
-  Error error_ = success;
-
-  FileIO(const FileIO&);
-  const FileIO& operator=(const FileIO&);
-};
-
-class FileIODummy : public FileIO {
- public:
-  FileIODummy() = default;
-  FileIODummy(const char* filename, uint32_t flg) { Open(filename, flg); }
-  ~FileIODummy() override = default;
-
-  bool Open(const std::string_view filename, uint32_t flg) override;
-  bool CreateNew(const std::string_view filename) override;
-  bool Reopen(uint32_t flg) override;
-  void Close() override;
-
-  int32_t Read(void* dest, int32_t len) override;
-  int32_t Write(const void* src, int32_t len) override;
-  bool Seek(int32_t fpos, SeekMethod method) override;
-  int32_t Tellp() override;
-  bool SetEndOfFile() override;
-
  private:
   FILE* fp_ = nullptr;
+
+  uint32_t flags_ = 0;
+  uint32_t origin_ = 0;
+  Error error_ = kSuccess;
 };
